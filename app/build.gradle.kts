f4957113eebbf2c5fa6b166f529271b45e29cff3
Michał Jaremczuk <michaljaremczuk@gmail.com>
2026-06-18 10:35:21 +0200

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.mjaremczuk.motiv"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
